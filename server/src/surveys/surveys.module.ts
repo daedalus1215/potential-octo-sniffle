62f86bccfed0e301c8fd73f502f538de35e02fc0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-04-05 23:16:48 -0400

    create and fetch are working

--- server/src/surveys/surveys.module.ts
+++ server/src/surveys/surveys.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SurveyController } from './survey-controller/survey.controller';
+import { SurveyService } from './services/survey.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Survey, SurveySchema } from './entity/survey.schema';
 
 @Module({
-    controllers: [SurveyController]
+    imports: [MongooseModule.forFeature([{name: Survey.name, schema: SurveySchema}])],
+    controllers: [SurveyController],
+    providers: [SurveyService],
 })
 export class SurveysModule { }
